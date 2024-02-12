3d063e30ab26c3ac72cba2de1aed31f978cee6eb
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-02-12 03:01:36 +0200

    add

---
+++ app/main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require('electron') 
+const url = require('url') 
+const path = require('path')  
+
+let win  
+
+function createWindow() { 
+   win = new BrowserWindow({width: 800, height: 600}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+}  
+
+
+app.on("window-all-closed", () => {  
+    if (process.platform !== "darwin") {  
+      app.quit();  
+    }  
+  });  
+  
+app.on('ready', createWindow) 
